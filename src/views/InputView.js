9d90d3f8142d57110ebcaa7866b9bc5d412a6fa2
brgndy <brgndy96@gmail.com>
2023-12-13 13:14:06 +0900

    Feat: 코치 이름 입력 받는 기능 구현

--- src/views/InputView.js
+++ src/views/InputView.js
@@ -1,3 +1,13 @@
-const InputView = {};
+import { Console } from '@woowacourse/mission-utils';
+import PROGRESS_MESSAGE from '../constants/messages/progressMessage.js';
+import DELIMITER from '../constants/delimiters/delimiter.js';
+
+const InputView = {
+  async readCoachNames() {
+    const coachNames = await Console.readLineAsync(PROGRESS_MESSAGE.read_coach_names);
+
+    return coachNames.split(DELIMITER.comma).map((name) => name.trim());
+  },
+};
 
 export default InputView;
