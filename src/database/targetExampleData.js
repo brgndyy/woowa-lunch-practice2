e4cb4dc3826fba8147bdecdcb0e691e755bc24e4
brgndy <brgndy96@gmail.com>
2023-12-14 07:24:03 +0900

    Feat: 최종적으로 반환해야하는 데이터 예시 작성

---
+++ src/database/targetExampleData.js
@@ -0,0 +1,102 @@
+// 최종적으로 반환해야하는 결과값 예시입니다.
+// 실질적으로 사용하진 않지만 비교를 하며 만들기 위해 만들어놓습니다.
+
+const targetExampleData = [
+  {
+    day: '월요일',
+    category: '한식',
+    categoryNumber: 2,
+    result: [
+      {
+        name: '토미',
+        menu: '쌈밥',
+      },
+      {
+        name: '제임스',
+        menu: '된장찌개',
+      },
+      {
+        name: '포코',
+        menu: '된장찌개',
+      },
+    ],
+  },
+  {
+    day: '화요일',
+    category: '한식',
+    categoryNumber: 2,
+    result: [
+      {
+        name: '토미',
+        menu: '김치찌개',
+      },
+      {
+        name: '제임스',
+        menu: '비빔밥',
+      },
+      {
+        name: '포코',
+        menu: '불고기',
+      },
+    ],
+  },
+  {
+    day: '수요일',
+    category: '일식',
+    categoryNumber: 1,
+    result: [
+      {
+        name: '토미',
+        menu: '미소시루',
+      },
+      {
+        name: '제임스',
+        menu: '가츠동',
+      },
+      {
+        name: '포코',
+        menu: '하이라이스',
+      },
+    ],
+  },
+  {
+    day: '목요일',
+    category: '중식',
+    categoryNumber: 3,
+    result: [
+      {
+        name: '토미',
+        menu: '짜장면',
+      },
+      {
+        name: '제임스',
+        menu: '토마토 달걀볶음',
+      },
+      {
+        name: '포코',
+        menu: '탕수육',
+      },
+    ],
+  },
+  {
+    day: '금요일',
+    category: '아시안',
+    categoryNumber: 4,
+    result: [
+      {
+        name: '토미',
+        menu: '팟타이',
+      },
+      {
+        name: '제임스',
+        menu: '파인애플 볶음밥',
+      },
+      {
+        name: '포코',
+        menu: '나시고렝',
+      },
+    ],
+  },
+];
+
+export default targetExampleData;
